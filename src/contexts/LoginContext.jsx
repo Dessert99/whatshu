02a7646cfc8cf717a6e17cb6032562b3f12a93a9
dessert99 <lsjh1234@naver.com>
2025-07-17 03:10:31 +0900

    오류 수정2

--- src/contexts/LoginContext.jsx
+++ src/contexts/LoginContext.jsx
@@ -3,17 +3,17 @@ import { useState, createContext } from 'react';
 export const LoginContext = createContext(null);
 
 export const LoginContextProvider = ({ children }) => {
-  // 상태
-  const [isLogin, setLogin] = useState(false);
+  // // 상태
+  // const [isLogin, setLogin] = useState(false);
 
-  const [userInfo, setUserInfo] = useState({});
+  // const [userInfo, setUserInfo] = useState({});
 
-  const [roles, setRoles] = useState();
+  // const [roles, setRoles] = useState();
 
-  //세팅
-  const loginSetting = (userData) => {};
+  // //세팅
+  // const loginSetting = (userData) => {};
 
-  const logoutSetting = (userData) => {};
+  // const logoutSetting = (userData) => {};
 
   //권한 정보 세팅
 
