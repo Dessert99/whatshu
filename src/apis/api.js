1a9ea82fab4b5b1fb874851dbbc3101e6792971c
dessert99 <lsjh1234@naver.com>
2025-07-24 15:28:24 +0900

    환경변수 제거

--- src/apis/api.js
+++ src/apis/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL,
+  baseURL: 'https://api-dev.whatshu.com',
   timeout: 5000,
   withCredentials: true,
 });
